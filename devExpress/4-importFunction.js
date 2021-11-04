75674a282260df96b1c50d2603889f7463bf6129
Kyle Whitten <kfw2@outlook.com>
2021-11-04 10:10:51 -0500

    Using ClientFunction & importing functions from another file

---
+++ devExpress/4-importFunction.js
@@ -0,0 +1,23 @@
+import { ClientFunction } from 'testcafe';
+
+// use require instread of import to avoid transpiling errrors
+const getDocumentURI = require('./utils.js').getDocumentURI;
+
+// Load DevExpress page
+fixture `My fixture`
+    .page `http://devexpress.github.io/testcafe/example/`;
+
+/*
+* Import Functions to be Used as Client Function Dependencies
+*/
+test('My test', async t => {
+    const getUri = ClientFunction(() => {
+        return getDocumentURI();
+    }, { dependencies: { getDocumentURI } });
+
+    const uri = await getUri();
+
+    await t
+        .maximizeWindow()
+        .expect(uri).eql('https://devexpress.github.io/testcafe/example/');
+});
--- devExpress/README.md
+++ devExpress/README.md
@@ -5,3 +5,4 @@
 - 1-emptyTest.js : file.js bones setup for testing with TestCafe.
 - 2-submit.js : Type developer name into text editor field, click 'Submit' and on the page that loads after clicking 'Submit' grab the innerText of result-content > h1.
 - 3-assertion.js : Type developer name into text editor field, click 'Submit' and using an assertion to check if the header text is equal to the expected text (from the input dev name).
+- 4-importFunction.js, utils.js : Practicing with ClientFunction and importing functions from other files.
